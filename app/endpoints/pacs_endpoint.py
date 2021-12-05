08218db7e7f2af749d69e8685677ced6c3f37839
tugot17 <piotr635@gmail.com>
2021-12-05 18:25:01 +0100

    Pydantic added

--- app/endpoints/pacs_endpoint.py
+++ app/endpoints/pacs_endpoint.py
@@ -3,6 +3,14 @@ from dicomweb_client.api import DICOMwebClient
 from app.segmentation.inference import get_lungs_masks
 from app.endpoints.utils import convert_single_class_mask_to_response_json
 import numpy as np
+from pydantic import BaseModel
+
+
+class PACSStudy(BaseModel):
+    server_address: str
+    study_instance_uid: str
+    series_instance_uid: str
+
 
 from app.docker_logs import get_logger
 
@@ -15,11 +23,11 @@ logger = get_logger("pacs-endpoint-logger")
 
 @router.post("/predict")
 async def predict(
-    server_address: str, study_instance_uid: str, series_instance_uid: str
+        pacs_study: PACSStudy
 ):
-    client = DICOMwebClient(url=server_address)
+    client = DICOMwebClient(url=pacs_study.server_address)
     instances = client.retrieve_series(
-        study_instance_uid=study_instance_uid, series_instance_uid=series_instance_uid
+        study_instance_uid=pacs_study.study_instance_uid, series_instance_uid=pacs_study.series_instance_uid
     )
     logger.info(f"{len(instances)} instances in series")
 
@@ -30,12 +38,12 @@ async def predict(
 
     masks = get_lungs_masks(series_array)
 
-    result = convert_single_class_mask_to_response_json(study_instance_uid,
-                                                      series_instance_uid,
-                                                      mapping_dict,
-                                                      masks.transpose(2, 0, 1),
-                                                      "Lungs",
-                                                      "Lungs")
+    result = convert_single_class_mask_to_response_json(pacs_study.study_instance_uid,
+                                                        pacs_study.series_instance_uid,
+                                                        mapping_dict,
+                                                        masks.transpose(2, 0, 1),
+                                                        "Lungs",
+                                                        "Lungs")
 
-    return result
-    # return {"Status": "success"}
+    # return result
+    return {"Status": "success"}
