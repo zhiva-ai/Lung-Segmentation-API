6d93ebad20adbb4c9dfcb83773e35c1561f305c5
tugot17 <piotr635@gmail.com>
2021-12-09 21:37:49 +0100

    example_request.py updated

--- example_request.py
+++ example_request.py
@@ -7,16 +7,16 @@ server_address = "https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs"
 study_instance_uid = "1.3.6.1.4.1.25403.345050719074.3824.20170125095438.5"
 series_instance_uid = "1.3.6.1.4.1.25403.345050719074.3824.20170125095449.8"
 
-data = {
+params = {
     "server_address": server_address,
     "study_instance_uid": study_instance_uid,
     "series_instance_uid": series_instance_uid,
 }
 
 if __name__ == '__main__':
-    result = requests.post(url, json=data)
-    data = result.json()
+    result = requests.post(url, json=params)
+    params = result.json()
     with open("data.json", "w") as f:
-        json.dump(data, f)
+        json.dump(params, f)
 
-    print(data)
+    print(params)
