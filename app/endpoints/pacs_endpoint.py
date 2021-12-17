67e084f2fa59abb5ecb51ca78afb295131a3cacf
tugot17 <piotr635@gmail.com>
2021-12-17 21:57:01 +0100

    Get method updated

--- app/endpoints/pacs_endpoint.py
+++ app/endpoints/pacs_endpoint.py
@@ -21,18 +21,22 @@ logger = get_logger("pacs-endpoint-logger")
 
 
 @router.get("/predict")
-async def predict(pacs_study: PACSStudy):
+async def predict(
+    server_address: str, study_instance_uid: str, series_instance_uid: str
+):
     """
     Lung segmentation endpoint. Takes the PACS server address, study and series UIDs as input,
     retrieves the DICOM image and provides it as input for the lung segmentation model. The mask predictions in the
     json is returned
-    :param pacs_study: Json with server_address, study_instance_uid, series_instance_uid
+    :param server_address:
+    :param study_instance_uid:
+    :param series_instance_uid:
     :return: json in the specified format
     """
-    client = DICOMwebClient(url=pacs_study.server_address)
+    client = DICOMwebClient(url=server_address)
     instances = client.retrieve_series(
-        study_instance_uid=pacs_study.study_instance_uid,
-        series_instance_uid=pacs_study.series_instance_uid,
+        study_instance_uid=study_instance_uid,
+        series_instance_uid=series_instance_uid,
     )
     logger.info(f"{len(instances)} instances in series")
 
@@ -46,8 +50,8 @@ async def predict(pacs_study: PACSStudy):
 
     # return {"Status": "success"}
     return convert_single_class_mask_to_response_json(
-        pacs_study.study_instance_uid,
-        pacs_study.series_instance_uid,
+        study_instance_uid,
+        series_instance_uid,
         mapping_dict,
         masks,
         "Lungs",
